bfa4809fcc9d88c80172cf6efef0b4531e2a6cd8
MattiLope <126561399+MattiLope@users.noreply.github.com>
2025-09-17 18:25:35 +0300

    Vaihe 2, Lisätty raha ja pelikierrokset

--- src/App.java
+++ src/App.java
@@ -8,18 +8,37 @@ public class App {
         Random q = new Random();
         Random p = new Random();
 
-        int n1 = r.nextInt(10)+1;
-        int n2 = q.nextInt(10)+1;
-        int n3 = p.nextInt(10)+1;
+        int money = 5;
 
-        System.out.println(n1);
-        System.out.println(n2);
-        System.out.println(n3);
+        
+        while (money > 0) {
+            
+            int n1 = r.nextInt(10)+1;
+            int n2 = q.nextInt(10)+1;
+            int n3 = p.nextInt(10)+1;
 
-        if (n1 == 7 || n2 == 7 || n3 == 7) {
-            System.out.println("Voitit!");
-        }else {
-            System.out.println("Hävisit!");
+            System.out.println(n1);
+            System.out.println(n2);
+            System.out.println(n3);
+            
+            if (n1 == 7 && n2 == 7 && n3 == 7) {
+                money += 7;
+                System.out.println("Voitit 7");
+            }else if(n1 == 7 && n2 == 7 && n3 != 7 ||
+                     n1 == 7 && n3 == 7 && n2 != 7 ||
+                     n2 == 7 && n3 == 7 && n1 != 7) {
+
+                        money += 5;
+                        System.out.println("Voitit 5 euroa");
+                     }else if(n1 == 7 || n2 == 7 || n3 == 7) {
+                        money += 3;
+                        System.out.println(money);
+                        System.out.println("Voitit 3 euroa");
+                     }else {
+                        money--;
+                        System.out.println(money);
+                        System.out.println("Hävisit");
+                     }             
         }
     }
 }
